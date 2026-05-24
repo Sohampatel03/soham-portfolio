b9fcecf9b2303d8ea03037de6924fbbe58bfdca7
sohampatel03 <soham171203@gmail.com>
2026-05-24 21:31:51 +0530

    add

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,8 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "B.Tech, Computer Science Engineering",
+    title: "B.Tech, Computer Science & Information Technology",
     duration: "2022 - 2026",
     institution: "IPS Academy, Indore — CGPA: 8.6",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Education",
+    duration: "2021 - 2022",
+    institution: "Collegiate School, Sanawad",
+  },
+  {
+    id: 3,
+    title: "Secondary Education",
+    duration: "2019 - 2020",
+    institution: "Max Academy, Dharampuri",
   },
 ]
